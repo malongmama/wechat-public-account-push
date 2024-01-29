200c21aeceabee252fddb1ecce71563be9fda9cc
malongmama <158006789+malongmama@users.noreply.github.com>
2024-01-29 15:05:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -55,7 +55,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'rNKJf-pntqVwHkaJ6oF7czGfFyIS7SCgE_vyixh6KVE',
+  CALLBACK_TEMPLATE_ID: 'BIdcZBHNGV6ytQVIOWdr1ITy-_3KP4ts_6OI-2ZZwh8',
 
   CALLBACK_USERS: [
     {
